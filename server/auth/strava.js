18e614f938c4d0cc86d9b75ce999da1df310046b
Spencer Collins <smcollins36@gmail.com>
2021-09-02 15:04:15 -0500

    created strava oauth file, added passport for strava oauth

---
+++ server/auth/strava.js
@@ -0,0 +1,20 @@
+const passport = require('passport')
+const router = require('express').Router()
+const StravaStrategy = require('@riderize/passport-strava-oauth2').Strategy
+const {User} = require('../db/models')
+module.exports = router
+
+passport.use(
+  new StravaStrategy(
+    {
+      clientID: process.env.STRAVA_CLIENT_ID,
+      clientSecret: process.env.STRAVA_CLIENT_SECRET,
+      callbackURL: 'localhost:8080'
+    },
+    function(accessToken, refreshToken, profile, done) {
+      User.findOrCreate({stravaId: profile.id}, function(err, user) {
+        return done(err, user)
+      })
+    }
+  )
+)
