f118b1ba811b20b75336f9b8583d31f2baad5acb
Spencer Collins <smcollins36@gmail.com>
2021-09-17 13:03:21 -0500

    created bikelist component

---
+++ client/components/bikeList.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const BikeList = props => {
+  return <div>List of all bikes</div>
+}
---
+++ client/store/bike.js
@@ -0,0 +1,7 @@
+import axios from 'axios'
+
+const GET_BIKES = 'GET_BIKES'
+
+const defaultBikes = {}
+
+//const getBikes =
